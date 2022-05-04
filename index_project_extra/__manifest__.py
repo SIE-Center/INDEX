fe40cdb9749d29dba636dc54f776a7a633fed83b
Alonsowski <santanasamuel29@gmail.com>
2022-05-04 17:57:01 -0500

    extra fields

---
+++ index_project_extra/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Project Extra (Index)",
+
+    'description': """
+        Funcionalidades extra a Projecto para Index Occidente.
+    """,
+
+    'author': "SIE Center / Samuel Santana",
+    'website': "http://www.siecenter.com.mx",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+   
+    'version': '15.0.1.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['project'],
+
+    # always loaded
+    'data': [
+        'views/project_task.xml',
+        'data/container_types.xml',
+        'data/packing_types.xml',
+        'data/service_types.xml',
+        # 'security/ir.model.access.csv',
+
+    ],
+}
---
+++ index_project_extra/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import extra_models
+from . import project_task
