533f5ee1f3ad4494c52d8866918c7e2507153ae1
Alonsowski <santanasamuel29@gmail.com>
2022-05-23 18:19:21 -0500

    move extra fields to new model

--- index_project_extra/models/__init__.py
+++ index_project_extra/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import extra_models
 from . import project_task
+from . import custom_task_line
--- index_project_extra/models/project_task.py
+++ index_project_extra/models/project_task.py
@@ -4,24 +4,4 @@ from odoo import models, fields
 class Tasks(models.Model):
     _inherit = "project.task"
 
-    custom_category = fields.Selection(
-        [('24','24 SIN PREVIO'),('36','36 CON PREVIO')], 
-        string="Category"
-    )
-    bl = fields.Char(string="Bill of Landing (BL)")
-    container_number = fields.Char()
-    agente_aduanal = fields.Char()
-    naviera = fields.Char()
-    forwarders = fields.Char()
-    operadora = fields.Char()
-    buque = fields.Char()
-    numero_viaje = fields.Char()
-    eta_date = fields.Datetime(string="Fecha ETA")
-    previo_date = fields.Datetime(string="Fecha PREVIO")
-    dispatch_date = fields.Datetime(string="Fecha Despacho")
-    peso = fields.Float()
-    pieza = fields.Char()
-
-    container_type_id = fields.Many2one('custom.container.type')
-    service_type_id = fields.Many2one('custom.service.type')
-    packing_type_id = fields.Many2one('custom.packing.type')
+    custom_task_line_ids = fields.One2many(comodel_name='custom.task.line', inverse_name='task_id')
